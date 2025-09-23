90b875d6dda7af5e3cd17f3a2787ddafc7fd35c7
Akiles Lima Ferreira <akilesxl35@gmail.com>
2025-09-22 21:07:50 -0300

    exercicio e pratica guiada sobre condicoes

---
+++ javascript/exercicios/Exercício – Descobrindo se a pessoa é maior ou menor de idade/idade.js
@@ -0,0 +1,8 @@
+var idade = 18
+if (idade >= 18){
+    console.log('Voce e maior de idade')
+} else {
+    console.log('Voce e menor de idade')
+}
+
+// testes feitos com os numeros: 15, 17, 19 e 20
---
+++ javascript/pratica-guiada/Prática Guiada – Testando condições no console/script.js
@@ -0,0 +1,12 @@
+/*var hora = 14
+if(hora < 12) {
+    console.log('Bom dia!')
+}// condicao simples
+*/ 
+
+var hora = 11
+if(hora < 12) {
+    console.log('Bom dia!')
+    } else {
+        console.log('Boa tarde!')
+    }
