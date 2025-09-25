c8dff37152aff0c57190f5a8d05fe915b46e1a05
Akiles Lima Ferreira <akilesxl35@gmail.com>
2025-09-25 19:58:08 -0300

    exercicios sobre while e do...while

---
+++ javascript/pratica-guiada/pratica-repeticoes/script.js
@@ -0,0 +1,12 @@
+/*
+var contador = 1
+while (contador <= 5){ 
+    console.log(A contagem esta em ${contador}) 
+    contador++ 
+} 
+*/
+var contador = 1
+do { 
+    console.log(`Contagem atua esta em ${contador}`)
+    contador++
+} while(contador <= 5)
