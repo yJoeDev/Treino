5cfd0bf81b74a4522ba7172ce8787f524b9f0f17
Akiles Lima Ferreira <akilesxl35@gmail.com>
2025-09-27 00:38:23 -0300

    desaifio sobre for

---
+++ javascript/desafios/desafio-for-tabuada-depuracao/script.js
@@ -0,0 +1,5 @@
+let numero = Number(window.prompt('Digite um Numero?'))
+for(let tabuada = 1; tabuada <= 10; tabuada++){
+    let multiplicar = tabuada * numero
+    let res = document.writeln(`respota de ${numero} x ${tabuada} = ${multiplicar} <br>`)
+}
